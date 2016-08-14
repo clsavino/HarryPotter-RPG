55981bcbcbf45b9509f4d31dc3ab6dd26d5c8349
Christi Savino <c.savino@ymail.com>
2016-08-13 21:30:01 -0400

    push to heroku

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,35 @@
 $(document).ready(function() {
 
-var	wizards = [
-	{
+var	wizards = {
+	harry:{
 		'name': 'harry',
 		'jpgFile': "assets/images/harry300x300.jpg",
-		'hp' : 200,
-		'ap' : 30,
-		'counterAttackPower' : 30
+		'hp' : 175,
+		'ap' : 25,
+		'counterAttackPower' : 25
 	},
-	{
+	draco:{
 		'name': 'draco',
 		'jpgFile': "assets/images/draco300x300.jpg",
 		'hp' : 150,
-		'ap' : 25,
-		'counterAttackPower' : 25		
-	},
-	{
-		'name': 'hermione',
-		'jpgFile': "assets/images/hermione300x300.png",
-		'hp' : 125,
 		'ap' : 20,
 		'counterAttackPower' : 20		
 	},
-	{
+	hermione:{
+		'name': 'hermione',
+		'jpgFile': "assets/images/hermione300x300.png",
+		'hp' : 125,
+		'ap' : 15,
+		'counterAttackPower' : 15		
+	},
+	ron:{
 		'name': 'ron',
 		'jpgFile': "assets/images/ron300x300.jpg",
 		'hp' : 100,
 		'ap' : 10,
-		'counterAttackPower' : 10	
-	}];//End of wizards array
+		'counterAttackPower' : 10
+	}	
+	};//End of wizards 
 
 	var gameOver = false;
 	var attackerSelected = false;
@@ -36,18 +37,19 @@ var	wizards = [
 	var defenderBeaten = false; 
 	var attacks = 0;
 	var wins = 0;
-	var disableAttackBtn = true;
-	var setupDone = true;
-	var counterAttackPower = 10;
+	var enableAttack = false;
 	var attackerSelected = false;
 	var defenderSelected = false;
-	var defenderAP = 10;
-	var defenderHP = 100;
-	var attackerHP = 100;
-	var attackerAP = 10;
 	var wizIndex = 0;
-
-	var yourAttackMsg = '<p>You attacked your enemy for ' + counterAttackPower + ' damage</p><p>Your enemy attacked you back for ' + defenderAP + ' damage</p>';
+	var attacker = "";
+	var defender = "";
+	var defenderAP;
+	var defenderHP;
+	var defenderName;
+	var attackerHP;
+	var attackerAP;
+	var attackerName;
+	var cloneImg;
 
 	var defeatedMsg = '<p>You have been defeated...Game Over. Press Restart to play again</p>';
 
@@ -57,44 +59,8 @@ var	wizards = [
 
 	var selectWizMsg = '<p>Select a Wizard to train for battle.</p><p>You must defeat the other 3 to win the Wizard Battle Contest.</p><p></p>';
 
-	function battle() {
-		console.log('battle function called');
-		// Attack button has been clicked
-		//update number of times attacker attacks 
-		//used to detmine counterAttackPower
-		attacks++;
-		console.log('attacks = ' + attacks);
-		counterAttackPower = attacks * wizards[wizIndex].ap;
-		console.log('counterAttackPower ' + counterAttackPower);
-		//display updated defender HP
-		defenderHP = defenderHP - counterAttackPower;
-		console.log('defenderHP ' + defenderHP);
-		//display updated attacker HP
-		attackerHP = attackerHP - defenderAP;
-		console.log('attackerHP ' + attackerHP);
-		//document.getElementById("gameInfo").innerHTML = '<p> </p>';
-		document.getElementById("gameInfo").innerHTML = yourAttackMsg; 
-		
-		if (defenderHP <= 0) {
-			document.getElementById('gameInfo').innerHTML= nextWizardMsg;
-			removeEnemy();
-			defenderSelected = false;
-			wins++;
-			disableAttackBtn = true;
-		}
-		if (attackerHP <= 0) {
-			document.getElementById('gameInfo').innerHTML = defeatedMsg;
-			disableAttackBtn = true;
-		}
-		if (wins === 3) {
-			document.getElementById('gameInfo').innerHTML = winnerMsg;
-		}
-
-		//
-	}
-
 	// move the attacker to the attacker location
-	function moveAttacker(){
+	function removeAttacker(){
 		console.log('moveAttacker function called');
 	}
 	// move the Defender to the defender location
@@ -117,15 +83,11 @@ var	wizards = [
 	// all things to do to restart a game
 	function restart() {
 		// remove attacker
-		moveAttacker();
+		removeAttacker();
 		//Put 4 images back at top
 		//$('.wizard-wrapper').show();
 		moveWizards();
-		// reset AP for all characters
-		wizards[0].counterAttackPower = 40;
-		wizards[1].counterAttackPower = 25;
-		wizards[2].counterAttackPower = 20;
-		wizards[3].counterAttackPower = 10;
+
 		// Set Select Wizard message
 		document.getElementById('gameInfo').innerHTML= nextWizardMsg;
 
@@ -137,34 +99,103 @@ var	wizards = [
 		defenderSelected = false;
 		defenderBeaten = false;
 		setupDone = true;
-		disableAttackBtn = true;
+		enableAttack = false;
 
 		console.log('restart function called');
 	}
+
+	function battle() {
+		console.log('battle function called');
+
+		//update number of times attacker attacks 
+		// attacks used to detmine counterAttackPower
+		attacks++;
+		console.log('attacks = ' + attacks);
+
+		counterAttackPower = attacks * attackerAP;
+		console.log('counterAttackPower ' + counterAttackPower);
+
+		//display updated defender HP
+		defenderHP = defenderHP - counterAttackPower;
+		console.log('defenderHP ' + defenderHP);
+
+		//display updated attacker HP
+		attackerHP = attackerHP - defenderAP;
+		console.log('defenderAP ' + defenderAP);
+		console.log('attackerHP ' + attackerHP);
+		console.log('======================');
+		//document.getElementById("gameInfo").innerHTML = '<p> </p>';
+		document.getElementById("gameInfo").innerHTML = '<p>You attacked your enemy for ' + counterAttackPower + ' damage</p><p>Your enemy attacked you back for ' + defenderAP + ' damage</p>'; 
+		
+		if (defenderHP <= 0) {
+			document.getElementById('gameInfo').innerHTML= nextWizardMsg;
+			removeEnemy();
+			defenderSelected = false;
+			wins++;
+			enableAttack = false;
+			console.log('wins ' + wins);
+			console.log('enableAttack ' + enableAttack);
+		}
+		if (attackerHP <= 0) {
+			document.getElementById('gameInfo').innerHTML = defeatedMsg;
+			enableAttack = false;
+		}
+		if (wins === 3) {
+			document.getElementById('gameInfo').innerHTML = winnerMsg;
+			enableAttack = false;
+		}
+
+	} //end of battle function
 
 	//listen for click on wizard
 	$('img').on('click', function() {
 		if (!attackerSelected) { 
 			console.log('attacker selected');
-		//no attacker selected yet
-			//get attacker's info using jQuery
-			// attacker = one clicked
-			// attackerHP = wizard[index].hp
-			// attackerAP = wizard[index].ap
-			moveAttacker();
+			//no attacker selected yet
+			attacker = $(this).data('name');
+			attackerHP = wizards[attacker].hp;
+			attackerAP = wizards[attacker].ap;
+			attackerName = wizards[attacker].name;
+			console.log('attacker ' + attacker);
+			//clone the image
+			cloneImg = $(this).clone();
+			//insert the clone
+			$(cloneImg).insertAfter('#box2');
+			$('#box2').append(cloneImg);
+			//prepend the name of defender
+			$(cloneImg).prepend('<p>' + attackerName + '</p');
+			//append the HP of the defender
+			$(cloneImg).append('<p>HP ' + attackerHP + '</p>');	
+			//remove the image
+			$(this).remove();
+
 			moveWizards();
 			attackerSelected = true;
 
 		} else {
 			if (!defenderSelected) {
 				console.log('defender selected');
-				//get defender's info 
-				//defender = one clicked
-				//defenderHP = wizard[index].hp
-				//defenderAP = wizard[index].ap
-				moveDefender();
+				defender = $(this).data('name');
+				console.log('defender ' + defender);
+				defenderHP = wizards[defender].hp;
+			    defenderAP = wizards[defender].ap;
+			    defenderName = wizards[defender].name;
+				//clone the image
+				cloneImg = $(this).clone();
+				//insert the clone
+				$('cloneImg').insertAfter('#box3');
+				$('#box3').append(cloneImg);
+
+				//prepend the name of defender
+				$(cloneImg).prepend('<p>' + defenderName + '</p');
+				//append the HP of the defender
+				$(cloneImg).append('<p>HP ' + defenderHP + '</p>');	
+				//remove the clicked image
+				$(this).remove();
+
 				defenderSelected = true;
-				disableAttackBtn = false;
+				enableAttack = true;
+				console.log('enableAttack ' + enableAttack);
 				
 			};
 		};//end of else
@@ -180,8 +211,11 @@ var	wizards = [
 	//listen for attack button clicked
 	$('#attackBtn').on('click', function() {
 		console.log('attack button clicked');
-		battle();
-			
+		console.log('enableAttack ' + enableAttack);
+		if (enableAttack = true) {
+			battle();
+		}
+					
 	});
 
 
